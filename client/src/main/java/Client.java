688a87c181e757636333d43f7ff3c99fec4d9f48
mmfay <mmfay3@gmail.com>
2023-05-20 19:45:21 -0700

    Add Client Startup Window

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,16 +1,24 @@
 import java.io.*;
 import java.net.*;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
 public class Client {
 
     private static Socket socket = null;
-    
+    private static JFrame mainFrame = null;
     public static void main(String[] args) {
         try {
             socket = new Socket("localhost",9000);
         } catch(Exception e) {
             System.out.println(e);
         }
+
+        mainFrame = new JFrame();
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        mainFrame.setVisible(true);
+
     }
 
 }
