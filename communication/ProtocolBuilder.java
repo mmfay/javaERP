0b45a2d9c97f91e5fa87b4107e3a1e1173a8c9ad
mmfay <mmfay3@gmail.com>
2023-05-26 19:51:33 -0700

    Add Protocol Class with Initial header

--- communication/ProtocolBuilder.java
+++ communication/ProtocolBuilder.java
@@ -1,8 +1,23 @@
+import org.json.*;
+import util.enums.HelperEnum;
+
 public class ProtocolBuilder {
 
-    public ProtocolBuilder() {
-        
+
+    public ProtocolBuilder(HelperEnum.ClientServer _type) {
+        if (_type == HelperEnum.ClientServer.server) {
+            buildHeaderServer();
+        } else {
+            buildHeaderClient();
+        }
     }
 
+    public void buildHeaderServer(){
+        //HelperEnu
+    }
+
+    public void buildHeaderClient(){
+
+    }
 
 }
