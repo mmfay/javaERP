b989750e4611fc53e67794dfee32cdb19d745e3a
mmfay <mmfay3@gmail.com>
2023-05-26 19:50:36 -0700

    Add Enum Import

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -3,11 +3,13 @@ import java.net.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import util.enums.HelperEnum;
 public class Client {
 
     private static Socket socket = null;
     private static JFrame mainFrame = null;
     public static void main(String[] args) {
+        
         try {
             socket = new Socket("localhost",9000);
         } catch(Exception e) {
@@ -16,7 +18,7 @@ public class Client {
 
         mainFrame = new JFrame();
         mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        
+
         mainFrame.setVisible(true);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
